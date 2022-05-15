94545ec81bc9266dab3f83eb0142ce965f84690b
Ilma133 <ilma.meskic.20@size.ba>
2022-05-15 22:00:57 +0200

    scroll top button

---
+++ script.js
@@ -0,0 +1,15 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 60){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
