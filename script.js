ea7faee032f86a9ddf32d1147a2c9a4e50d6a5c7
Ilma133 <ilma.meskic.20@size.ba>
2022-05-16 18:43:06 +0200

    Put metoda, forma u html, css update

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food`)
             </div>
             <div>
             <button type="button" class="btn btn-danger" onclick="obrisiJelo(${hrana.id})">Izbriši</button>
+            <button type="button" class="btn btn-danger" onclick="editJelo(${hrana.id})">Update</button>
             </div>
             </div>`;
           });
@@ -56,4 +57,44 @@ fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food`)
     .then(res => {
         console.log(res);
     })
+}
+
+const editJelo = (hranaId) => {
+  const hrana = jela.find(hrana => hrana.id === hranaId);
+  const jeloFormId = document.getElementById('obrok-id')
+  const jeloFormNaziv = document.getElementById('obrok-naziv')
+  const jeloFormCijena = document.getElementById('obrok-cijena')
+  const jeloFormURL = document.getElementById('obrok-url')
+
+  jeloFormId.value = hrana.id;
+  jeloFormNaziv.value = hrana.name;
+  jeloFormCijena.value = hrana.price;
+  jeloFormURL.value = hrana.imageUrl;
+}
+
+const updateJelo = () => {
+  const jeloFormId = document.getElementById('obrok-id').value;
+  const jeloFormNaziv = document.getElementById('obrok-naziv').value;
+  const jeloFormCijena = document.getElementById('obrok-cijena').value;
+  const jeloFormURL = document.getElementById('obrok-url').value;
+
+  fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food`, {
+      method: 'PUT',
+      headers: new Headers({
+          'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify ({
+          id: jeloFormId,
+          name: jeloFormNaziv,
+          price: jeloFormCijena,
+          imageUrl: jeloFormURL
+      })
+  })
+  .then(response => {
+      if(!response.ok)
+      {
+          alert('[ERROR]');
+      }
+  })
+
 }
